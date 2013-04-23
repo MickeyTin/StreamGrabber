be4d8b6268115dfffe4e89e618a84ce6327184f2
Mikhailo <irenobrowino@gmail.com>
2013-04-23 18:52:07 +0300

    Formatting performed

--- src/com/streamgrabber/structures/YaMusicTrackInfo.java
+++ src/com/streamgrabber/structures/YaMusicTrackInfo.java
@@ -1,6 +1,6 @@
 package com.streamgrabber.structures;
 
-public class YaMusicTrackInfo implements IMusicTrackInfo{
+public class YaMusicTrackInfo implements IMusicTrackInfo {
 
 	private String trackId;
 	private String storageDir;
@@ -11,10 +11,10 @@ public class YaMusicTrackInfo implements IMusicTrackInfo{
 	private String albumId;
 	private String duration;
 	private String coverURL;
-			
+
 	public YaMusicTrackInfo(String trackId, String storageDir, String title,
 			String artist, String artistId, String album, String albumId,
-			String duration, String coverURL) {		
+			String duration, String coverURL) {
 		this.trackId = trackId;
 		this.storageDir = storageDir;
 		this.title = title;
@@ -25,72 +25,89 @@ public class YaMusicTrackInfo implements IMusicTrackInfo{
 		this.duration = duration;
 		this.coverURL = coverURL;
 	}
+
 	@Override
 	public String getTrackId() {
 		return trackId;
 	}
+
 	public void setTrackId(String trackId) {
 		this.trackId = trackId;
 	}
+
 	public String getStorageDir() {
 		return storageDir;
 	}
+
 	public void setStorageDir(String storageDir) {
 		this.storageDir = storageDir;
 	}
+
 	@Override
 	public String getTitle() {
 		return title;
 	}
+
 	public void setTitle(String title) {
 		this.title = title;
 	}
+
 	@Override
 	public String getArtist() {
 		return artist;
 	}
+
 	public void setArtist(String artist) {
 		this.artist = artist;
 	}
+
 	public String getArtistId() {
 		return artistId;
 	}
+
 	public void setArtistId(String artistId) {
 		this.artistId = artistId;
 	}
+
 	@Override
 	public String getAlbum() {
 		return album;
 	}
+
 	public void setAlbum(String album) {
 		this.album = album;
 	}
+
 	public String getAlbumId() {
 		return albumId;
 	}
+
 	public void setAlbumId(String albumId) {
 		this.albumId = albumId;
 	}
+
 	@Override
 	public String getDuration() {
 		return duration;
 	}
+
 	public void setDuration(String duration) {
 		this.duration = duration;
 	}
+
 	public String getCoverURL() {
 		return coverURL;
 	}
+
 	public void setCoverURL(String coverURL) {
 		this.coverURL = coverURL;
-	}		
-	
-	
+	}
+
 }
-/*{"id":"9361356", "storage_dir":"415b1daa.9361356", 
- * "title":"Bitch", "artist":"Marc van Linden", "artist_id":"502793", 
- * "artist_var":"", "album":"Turn On The Radio, Vol. 7 - Club Music Radio Cuts And Edits", 
- * "album_id":"991116", "duration":"195000", 
+/*
+ * {"id":"9361356", "storage_dir":"415b1daa.9361356", "title":"Bitch",
+ * "artist":"Marc van Linden", "artist_id":"502793", "artist_var":"",
+ * "album":"Turn On The Radio, Vol. 7 - Club Music Radio Cuts And Edits",
+ * "album_id":"991116", "duration":"195000",
  * "cover":"http:\/\/storage.music.yandex.ru\/get\/0608c8f6.a.991116\/1.30x30.jpg"}
- * */
- 
+ */
